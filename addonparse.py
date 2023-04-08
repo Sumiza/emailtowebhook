12e2065a6e345d595584efd9b7bc81ae41ea51da
Sumiza <64884509+Sumiza@users.noreply.github.com>
2023-04-08 14:42:46 -0400

    Create addonparse.py

---
+++ addonparse.py
@@ -0,0 +1,19 @@
+class Send():
+    def __init__(self,
+    email,
+    dkimverify,
+    session,envelope,
+    email_dict,
+    hmactime,
+    hmac_digest,
+    webhook_headers) -> str:
+        
+        self.email = email
+        self.dkimverify = dkimverify
+        self.session = session
+        self.envelope = envelope
+        self.email_dict = email_dict
+        self.hmactime = hmactime
+        self.hmac_digest = hmac_digest
+        self.webhook_headers = webhook_headers
+        return '250 Message accepted'
