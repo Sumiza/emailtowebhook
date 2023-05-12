74b14e3b1f37bd84ecea1a9099fdfd1a602f028a
Sumiza <64884509+Sumiza@users.noreply.github.com>
2023-05-11 21:10:35 -0400

    openssl added

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,16 +2,21 @@
 
 if [ -n "$PIP_INSTALL" ]
 then
-        pip install --upgrade pip
-        for i in $PIP_INSTALL
-                do
-                        pip install "$i"
-                done
+    pip install --upgrade pip
+    for i in $PIP_INSTALL
+        do
+            pip install "$i"
+        done
 fi
 
 if [ -n "$BUILTIN_ADDON" ]
 then
-    mv /addons/$BUILTIN_ADDON/addon.py .
+    cp /addons/$BUILTIN_ADDON/addon.py .
+fi
+
+if [ -n "$TLS_CERT_HOST" ]
+then
+    openssl req -x509 -newkey rsa:4096 -keyout key.pem -out cert.pem -days 3000 -nodes -subj "/CN=$TLS_CERT_HOST"
 fi
 
 exec "$@"
